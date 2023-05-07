c7c73bac57dcae8df8beadb77a31cabef43da6a0
stanley <stanley.walkerv@gmail.com>
2023-05-07 18:38:36 -0400

    heroku bugs

--- public/server.js
+++ public/server.js
@@ -3,10 +3,22 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use((req, res, next) => {
+  if (req.url === '/') {
+    res.sendFile(path.join(__dirname, '/public/index.html'));
+  } else {
+    express.static(path.join(__dirname, 'public'))(req, res, next);
+  }
+});
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '/index.html'));
+app.get('*.js', function (req, res, next) {
+  res.setHeader('Content-Type', 'text/javascript');
+  express.static(path.join(__dirname, '/public'))(req, res, next);
+});
+
+app.get('*.css', function (req, res, next) {
+  res.setHeader('Content-Type', 'text/css');
+  express.static(path.join(__dirname, '/public'))(req, res, next);
 });
 
 app.listen(PORT, () => {
