be98196d2d86fb762dd05c86a80cd46086911d80
stanley <stanley.walkerv@gmail.com>
2023-05-12 03:15:26 -0400

    ui/ux improvements

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # growing-canvas
+# stablediffusion-infinity-creative-sandbox
+
+Infitnite/space is an interactive space for people to come together and collaborate on growning an image. The foundation of this project is based on this repo [stablediffusion-infinity by lkwq007](https://github.com/lkwq007/stablediffusion-infinity) and the concept is inspired by the Reddit project r/place - with the goal to foster creatvity and collaboration among people with generative AI. 
+
+next steps:
+- private launch and configure with hugging face
+- rearchitecture tool bar into styled css buttons/minimalistic design
+- intergrate liveblocks.io
+- redesign ui/ux
+- draw box moves window frame
+
+
+
+![outpaint](infinite-place-hackathon-demo.jpeg)
+
--- public/script.js
+++ public/script.js
@@ -1,19 +1,19 @@
-function animateSVG() {
-    gsap.to(".svg-element", {
-        duration: 1,
-        ease: "power1.inOut",
-        x: function() { return (Math.random() * 40) - 20; },
-        y: function() { return (Math.random() * 40) - 20; }
+// function animateSVG() {
+//     gsap.to(".svg-element", {
+//         duration: 1,
+//         ease: "power1.inOut",
+//         x: function() { return (Math.random() * 40) - 20; },
+//         y: function() { return (Math.random() * 40) - 20; }
 
-    });
-}
+//     });
+// }
 
 
 
 const colorClasses = ['color1', 'color2', 'color3', 'color4', 'color5'];
 
 $(".next").on("click", function() {
-    animateSVG();
+    
 
     let current = $(".slide.active");
     let next = current.next(".slide");
@@ -45,13 +45,73 @@ function removeSvgContainer() {
     }
 }
 
-// document.addEventListener('WebComponentsReady', function() {
-//     const gradioAppElement = document.querySelector('gradio-app');
-//     const textboxElement = gradioAppElement.shadowRoot.querySelector('[data-testid="textbox"]');
+$(document).ready(function() {
+    $(".next").click(function() {
+      let currentStep = $(".slide.active").data("step");
+      if (currentStep === 4) {
+        $(".next").hide();
+        $(".progress-bar").hide();
+      } else {
+        $(".next").show();
+        $(".progress-bar").show();
+      }
+    });
+  });
 
-//     if (textboxElement) {
-//         console.log("Textbox found:", textboxElement);
-//     } else {
-//         console.log("Textbox not found");
-//     }
-// });
+  function updateNextButtonAndProgressBarVisibility() {
+    let currentStep = $(".slide.active").data("step");
+    let totalSlides = $(".slide").length;
+    if (currentStep === totalSlides - 1) {
+        $(".next").hide();
+        $(".progress-bar").hide();
+    } else {
+        $(".next").show();
+        $(".progress-bar").show();
+    }
+}
+
+// Show the "Back" button when not on the first slide
+function updateBackButtonVisibility() {
+    let currentStep = $(".slide.active").data("step");
+    if (currentStep === 0) {
+      $(".back").hide();
+    } else {
+      $(".back").show();
+    }
+  }
+  
+  // Handle the "Back" button click event
+  $(".back").on("click", function() {
+    let current = $(".slide.active");
+    let prev = current.prev(".slide");
+  
+    if (prev.length) {
+      let currentIndex = colorClasses.indexOf(current[0].classList[2]);
+      let prevIndex = (currentIndex - 1 + colorClasses.length) % colorClasses.length;
+      current.removeClass(colorClasses[currentIndex]).addClass(colorClasses[prevIndex]);
+  
+      current.removeClass("active");
+      prev.addClass("active");
+  
+      let step = prev.data("step");
+      $(".circle.active").removeClass("active");
+      $(".circle[data-step=" + step + "]").addClass("active");
+    }
+  
+    updateBackButtonVisibility();
+    updateNextButtonAndProgressBarVisibility();
+  });
+  
+  // Update the "Back" button visibility when the "Next" button is clicked
+  $(".next").on("click", function() {
+    // ... (existing code) ...
+    updateBackButtonVisibility();
+    updateNextButtonAndProgressBarVisibility();
+    
+  });
+  
+  // Initialize the "Back" button visibility on page load
+  $(document).ready(function() {
+    updateBackButtonVisibility();
+    updateNextButtonAndProgressBarVisibility();
+  });
